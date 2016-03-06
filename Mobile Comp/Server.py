8f1b142b715dd70eb893de9fd5e2cd11678edc06
Ben <syny56@gmail.com>
2016-03-06 20:31:00 +0000

    Item GET implemented

--- Mobile Comp/Server.py
+++ Mobile Comp/Server.py
@@ -10,7 +10,7 @@ _cursor = _db.cursor()
 class databaseRequestHandler(tornado.web.RequestHandler):
 	def delete(self):
          _cursor.execute("DROP TABLE IF EXISTS data")
-         _cursor.execute("CREATE TABLE data (item STRING, price FLOAT(3), quantity INT)")
+         _cursor.execute("CREATE TABLE data (item STRING, price REAL, quantity INT)")
          milk_values = ["milk", None, None]
          ball_values = ["ball", None, None]
          _cursor.execute("INSERT INTO data VALUES (?,?,?)", milk_values)
