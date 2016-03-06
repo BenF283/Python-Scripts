ef8077ea0c45488b496fb0685f0d1cabac4669e5
Ben <syny56@gmail.com>
2016-03-06 19:00:58 +0000

    Item GET implemented

--- Mobile Comp/Server.py
+++ Mobile Comp/Server.py
@@ -20,10 +20,26 @@ class databaseRequestHandler(tornado.web.RequestHandler):
          
 class itemRequestHandler(tornado.web.RequestHandler):
     def put(self, item):
-        record = (self.get_argument("price"), item)
-        _cursor.execute("UPDATE data SET price=? WHERE item=?", record)
-        _db.commit()
-        self.write('OK')
+        price = self.get_argument("price")
+        quantity = self.get_argument("quantity")
+        if price != None:
+            record = (price, item)
+            _cursor.execute("UPDATE data SET price=? WHERE item=?", record)
+            _db.commit()
+            self.write('OK')
+        elif quantity != None:
+            record = (quantity, item)
+            _cursor.execute("UPDATE data SET quantity=? WHERE item=?", record)
+            _db.commit()
+            self.write('OK')
+    def get(self, item):
+        ans_price = self.get_price("price")        
+        ans_quantity = self.get_argument("quantity")
+
+        if ans_price == 'true':
+            self.write(item+" total stock value: "+_cursor.execute("SELECT price FROM data WHERE item=?", item))
+        elif ans_quantity == 'true':
+            self.write(item+" total stock value: "+_cursor.execute("SELECT quantity FROM data WHERE item=?", item))
         
             
 
