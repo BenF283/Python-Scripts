9d0016374fd90ae1ab135881391b4d5be4681176
Ubuntu <pythonserv@MobComp2.xtgdfntlgylendloysj4owdo3b.ax.internal.cloudapp.net>
2016-03-11 12:57:48 +0000

    Working mobile server

--- Mobile Comp/Server.py
+++ Mobile Comp/Server.py
@@ -18,34 +18,51 @@ class databaseRequestHandler(tornado.web.RequestHandler):
         _db.commit()
         self.write('OK')
     def get(self):
-        _cursor.execute("SELECT * FROM sqlite_master")
-        for row in _cursor:
-            self.write(row[0])
+        for line in _db.iterdump():
+            self.write(line)
          
          
 class itemRequestHandler(tornado.web.RequestHandler):
     def put(self, item):
-      
-        if self.get_argument("price") != None:
-            record = (self.get_argument("price"), item)
-            _cursor.execute("UPDATE data SET price=? WHERE item=?", record)
-            _db.commit()
-            self.write('OK')
-        elif self.get_argument("quantity") != None:
-            record = (self.get_argument("quantity"), item)
-            _cursor.execute("UPDATE data SET quantity=? WHERE item=?", record)
-            _db.commit()
-            self.write('OK')
+	_cursor.execute("SELECT * FROM data WHERE item=?", (item,))
+ 	result = _cursor.fetchone()
+	if result != None:
+	        if self.get_argument("price","x") != "x":
+                    record = (self.get_argument("price"), item)
+                    _cursor.execute("UPDATE data SET price=? WHERE item=?", record)
+                    _db.commit()
+                    self.write('OK')
+     		elif self.get_argument("quantity", "x") != "x":
+            		record = (self.get_argument("quantity"), item)
+          		_cursor.execute("UPDATE data SET quantity=? WHERE item=?", record)
+            		_db.commit()
+		        self.write('OK')
+	else:
+		self.set_status(404)
+	
     def get(self, item):
-        if self.get_argument("price") == 'true':
+	_cursor.execute("SELECT * FROM data WHERE item=?", (item,))
+	result = _cursor.fetchone()
+	if result != None:
+	        if self.get_argument("price", "x") == 'true':
 	    
-            _cursor.execute("SELECT price FROM data WHERE item=?", (item,))
-	    for row in _cursor:
-                self.write(item+" unit price: "+str(row[0]))
-        elif self.get_argument("quantity") == 'true':
-            _cursor.execute("SELECT quantity FROM data WHERE item=(?)", (item,))            
-	    for row in _cursor:
-                self.write(item+" stock level: "+str(row[0]))
+        	    _cursor.execute("SELECT price FROM data WHERE item=?", (item,))
+	    	    for row in _cursor:
+        	    	self.write(item+" unit price: "+str("{0:.2f}".format(row[0])))
+	        elif self.get_argument("quantity", "x") == 'true':
+        	    _cursor.execute("SELECT quantity FROM data WHERE item=(?)", (item,))            
+	    	    for row in _cursor:
+		        self.write(item+" stock level: "+str(row[0]))
+		elif self.get_argument("value", "x") == 'true':
+		    _cursor.execute("SELECT quantity FROM data WHERE item=(?)", (item,))
+		    for row in _cursor:
+			quantity = row[0]
+		    _cursor.execute("SELECT price FROM data WHERE item=(?)", (item,))
+		    for row in _cursor:
+			price = row[0]
+	    	    self.write(item+" total stock value: " + str("{0:.2f}".format(price * quantity)))
+	else:
+	        self.set_status(404)
         
 
 application = tornado.web.Application([
